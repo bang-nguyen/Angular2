f25728c4c30d0e9343376ba88d32e762dc4b213f
Bang Anh Nguyen <bang.nguyen@niteco.se>
2016-06-17 17:54:11 +0700

    Add feedback

---
+++ Feedback/main.js
@@ -0,0 +1,75 @@
+(function (document) {
+    var Q = function (selector) {
+        return new Library(selector);
+    }
+
+    var Library = function (selector) {
+        var elements = document.querySelectorAll(selector);
+
+        this.length = elements.length;
+        this.credit = {
+            version: "0.0.1",
+            author: ".||."
+        };
+
+        for (var i = 0; i < this.length; i++) {
+            this[i] = elements[i];
+        }
+
+        return this;
+    }
+
+    Q.fn = Library.prototype =
+        {
+            hide: function () {
+                var len = this.length;
+                while (len--) {
+                    this[len].style.display = 'none';
+                }
+                return this;
+            },
+            show: function () {
+                var len = this.length;
+                while (len--) {
+                    this[len].style.display = 'block';
+                }
+                return this;
+            },
+            addClass: function (className) {
+                var len = this.length;
+                while (len--) {
+                    this[len].className += " " + className;
+                }
+            },
+            removeClass: function (className) {
+                var len = this.length;
+                while (len--) {
+                    var regex = new RegExp('(\\s|^)' + className + '(\\s|$)');
+                    this.instance.className.replace(regex, '');
+                }
+            }
+        };
+
+    if (!window.Q) {
+        window.Q = Q;
+    }
+
+})(window.document);
+
+
+
+var ZooUtil = ZooUtil || {
+    createElement: function (name, attrs) {
+        var el = document.createElement(name);
+        for (var name in attrs) {
+            var value = attrs[name];
+            el.setAttribute(name, value);
+        }
+        return el;
+    }
+}
+
+
+
+//var myDiv = ZooUI(null).createElement("div", { class: "my-div", id: "my-id" })
+
